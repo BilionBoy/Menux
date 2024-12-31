da674473d99b7c395f73bd76e79fe0970f669422
BilionBoy <dhiogoandre26@gmail.com>
2024-12-31 13:24:42 -0400

    refactor: layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,49 +1,5 @@
 class HomeController < ApplicationController
+  
   def index
-  end
-
-  def conteudo
-  end
-
-  # Métodos dos Components
-  def avatars
-  end
-
-  def buttoes
-  end
-
-  def fontes
-  end
-
-  def gridsystem
-  end
-
-  def notificacoes
-  end
-
-  def paineis
-  end
-
-  def simpleicons
-  end
-
-  def alertas
-  end
-
-  def tipografia
-  end
-
-  # Métodos dos Graficos
-  def grafico
-  end
-
-  def minigrafico
-  end
-
-  # Métodos das Tables
-  def datatables
-  end
-
-  def tabelas
   end
 end
