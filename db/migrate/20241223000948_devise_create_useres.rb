76b929105dcb8aa2dabcac2db292a240c1d1e2ae
BilionBoy <dhiogoandre26@gmail.com>
2025-01-03 09:19:15 -0400

    adicionando role ao db devise

--- db/migrate/20241223000948_devise_create_useres.rb
+++ db/migrate/20241223000948_devise_create_useres.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
-class DeviseCreateUseres < ActiveRecord::Migration[7.2]
+class DeviseCreateUsers < ActiveRecord::Migration[7.2]
   def change
-    create_table :useres do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -32,13 +30,12 @@ class DeviseCreateUseres < ActiveRecord::Migration[7.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
     end
 
-    add_index :useres, :email,                unique: true
-    add_index :useres, :reset_password_token, unique: true
-    # add_index :useres, :confirmation_token,   unique: true
-    # add_index :useres, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
