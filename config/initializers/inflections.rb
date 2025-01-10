f9501f511a278e26093d8c1a40f275824bef1000
BilionBoy <dhiogoandre26@gmail.com>
2025-01-10 09:34:12 -0400

    Inflections

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -3,12 +3,17 @@
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.plural /^(ox)$/i, "\\1en"
-#   inflect.singular /^(ox)en/i, "\\1"
-#   inflect.irregular "person", "people"
-#   inflect.uncountable %w( fish sheep )
-# end
+# config/initializers/inflections.rb
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.irregular "user_estabelecimento", "users_estabelecimentos"
+  inflect.irregular "funcao", "funcoes"
+  inflect.irregular "produto", "produtos"
+  inflect.irregular "categoria", "categorias"
+  inflect.irregular "item_pedido", "itens_pedidos"
+  inflect.irregular "avaliacao", "avaliacoes"
+end
+
 
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
