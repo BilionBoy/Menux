080e5fc224098602db81439866484cea4db2555a
BilionBoy <dhiogoandre26@gmail.com>
2025-01-11 16:20:22 -0400

    Inflections

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,8 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
-# Add new inflection rules using the following format. Inflections
-# are locale specific, and you may define rules for as many different
-# locales as you wish. All of these examples are active by default:
 # config/initializers/inflections.rb
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
@@ -12,10 +7,12 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular "categoria", "categorias"
   inflect.irregular "item_pedido", "itens_pedidos"
   inflect.irregular "avaliacao", "avaliacoes"
+  inflect.irregular "estabelecimento", "estabelecimentos"
+  inflect.irregular "categoria_produto", "categoria_produtos"
+  inflect.irregular "cliente", "clientes"
+  inflect.irregular "status_pedido", "status_pedidos"
+  inflect.irregular "pedido", "pedidos"
+  inflect.irregular "venda", "vendas"
+  inflect.irregular "vendas_item_pedido", "vendas_item_pedidos"
+  inflect.irregular "ticket", "tickets"
 end
-
-
-# These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym "RESTful"
-# end
