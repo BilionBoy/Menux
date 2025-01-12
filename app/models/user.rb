c92733cee64f3e6e48ab32240dcbf33be7d5694d
BilionBoy <dhiogoandre26@gmail.com>
2025-01-12 17:12:52 -0400

    Devise: model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Devise Modules
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
+
+  # Associations
+  belongs_to :funcao
+
+  # Validations
+  validates :nome, presence: true
+  validates :cpf, presence: true, uniqueness: true, format: { with: /\A\d{11}\z/, message: "deve conter 11 dígitos numéricos" }
+  validates :telefone, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :funcoes
   root "home#index"
   get "home/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :funcoes
 
+  # Status HTTP da aplicação
   get "up" => "rails/health#show", as: :rails_health_check
 end
