d6e47dad1b785501a1c9643652e3529f8f05fa8f
BilionBoy <dhiogoandre26@gmail.com>
2025-01-12 17:11:31 -0400

    feature: devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_12_065355) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_12_210911) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,4 +20,25 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_12_065355) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "cpf", null: false
+    t.string "telefone"
+    t.string "nome"
+    t.bigint "funcao_id"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cpf"], name: "index_users_on_cpf", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["funcao_id"], name: "index_users_on_funcao_id"
+  end
+
+  add_foreign_key "users", "funcoes"
 end
