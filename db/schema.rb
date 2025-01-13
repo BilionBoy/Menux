88de6017d9ea425bfe10740728b948ce8e058b7f
BilionBoy <dhiogoandre26@gmail.com>
2025-01-12 21:54:31 -0400

    Feature: estabelecimentos

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_12_213205) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_13_012041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "estabelecimentos", force: :cascade do |t|
+    t.string "nome", null: false
+    t.string "cnpj", null: false
+    t.text "endereco"
+    t.string "telefone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cnpj"], name: "index_estabelecimentos_on_cnpj", unique: true
+  end
 
   create_table "funcoes", force: :cascade do |t|
     t.string "nome"
